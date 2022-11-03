827e448cbbf3ad18ac0b0c5855e2be3a99f9c8bf
sahba atarodi <sahba.atarodi@gmail.com>
2022-11-03 18:45:05 -0400

    finished profile config

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, What is your first name? ");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {userName}, What is your favorite color? ");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is awesome, What is your favorite animal? ");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now  What is your favorite band? ");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"Thanks {userName}! Here is your profile");
+            Console.WriteLine("------------------------------------");
+
+            Console.WriteLine($"Name : {userName}");
+            Console.WriteLine($"Favorite color : {color}");
+            Console.WriteLine($"Favorite animal : {animal}");
+            Console.WriteLine($"Favorite Band : {band}");
+
         }
     }
 }
