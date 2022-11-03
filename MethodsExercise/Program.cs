a64687fe27d6faa3c34803adb7d089a9569d20b2
sahba atarodi <sahba.atarodi@gmail.com>
2022-11-03 18:59:33 -0400

    final program

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,8 +2,24 @@
 {
     public class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 + num3;
+        }
         static void Main(string[] args)
         {
+
+            var amountOfCars = Sum(2, 6);
+            var blah = Multiply(60, 2, 4);
+           
+            
+            //------------exercise 1 -------------
+
             Console.WriteLine("Hello, What is your first name? ");
             var userName = Console.ReadLine();
 
@@ -23,6 +39,8 @@
             Console.WriteLine($"Favorite color : {color}");
             Console.WriteLine($"Favorite animal : {animal}");
             Console.WriteLine($"Favorite Band : {band}");
+
+            //------------exercise 2--------------
 
         }
     }
